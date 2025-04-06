b37729fc4bca745624e54be72d9ef203fe2a0e76
AxelCC <axelcarreto9@gmail.com>
2025-04-05 22:53:45 -0600

    :sparkles: mi primer codigo

--- 01.Estructuras_de_control/main.js
+++ 01.Estructuras_de_control/main.js
@@ -1,9 +1,13 @@
+// aqui es donde se trabaja toda la logica
+
+// vamos a hacer un programa donde guardemos la edad en una variable y sepamos si es mayor o menor de edad
+
 
 /* 1 Declaro una variable*/
-let age = 20;
+let age = 17;
 //implemento una condicional o estructura de control//
 if(age >= 18){
     console.log("ERES MAYOR DE EDAD");
 }else{
-     console.log("Eres menor de edad");   
+     console.log("ERES MENOR DE EDAD");   
 }
