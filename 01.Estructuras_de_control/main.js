54fc73170332dd636d71b63589f71e4101580c7d
AxelCC <axelcarreto9@gmail.com>
2025-04-05 21:42:01 -0600

    :sparkles: Template base de html

---
+++ 01.Estructuras_de_control/main.js
@@ -0,0 +1,9 @@
+
+/* 1 Declaro una variable*/
+let age = 20;
+//implemento una condicional o estructura de control//
+if(age >= 18){
+    console.log("ERES MAYOR DE EDAD");
+}else{
+     console.log("Eres menor de edad");   
+}
